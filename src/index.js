32e496b9d1c18864feea198dae4b7cdbb7c5284e
Lorenzo Sorrells <lsorrells1996@gmail.com>
2022-01-19 09:22:25 -0600

    made a function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function getImagesFromServer(url) {
+    return fetch(imgUrl)
+        .then(r => r.json())
+        .then(obj => obj.message)
+}
+
+
+// get rid of the chunk if it doesnt work 
+
+
+// fetch(imgUrl).then(r => r.json()).then(obj => obj.message[0])
